05cc84c08b58191b4e338ce3cc2ac576cdec00b9
nguyenquy42 <nguyenquy26012014@gmail.com>
2020-08-17 08:48:39 +0700

    up bài A câu 2

--- js/script3.js
+++ js/script3.js
@@ -5,7 +5,7 @@ function Tinh() {
 }
 function merge2String(A, B) {
     var C = "";
-    var len = A.length + B.length
+    var len = A.length + B.length;
     for (var i = 0; i < len; i++) {
         if (A[i] != null) {
             C += A[i];
